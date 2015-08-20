50bb6e34422a0be24f1761befafb3b05dee815df
Arenlor <arenlor@arenlor.com>
2015-08-20 08:07:21 -0400

    Simple Storage update
    Switched to Simple Storage for the counter, prepared for apikey and url white list.
    
    Signed-off-by: Arenlor <arenlor@arenlor.com>

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+var ss = require("sdk/simple-storage");
 var preferences = require("sdk/simple-prefs").prefs;
 var { setTimeout } = require("sdk/timers");
 var panel;
+
+// check to see if storage exists
+if(!ss.storage.visitCounter) {
+    ss.storage.visitCounter = 0;
+    // Tracks the visitCounter
+}
+if(!ss.storage.apikey) {
+    ss.storage.apikey = "";
+    // The apikey used
+}
+if(!ss.storage.urlwhitelist) {
+    ss.storage.urlwhitelist = [];
+    // An array for the urls
+}
 
 // On load see if the page is in whitelist, if not increment counter.
 require("sdk/tabs").on("ready", checkPage);
 function checkPage(tab) {
     if(true) {
-        preferences.visitCounter = preferences.visitCounter + 1;
+        ss.storage.visitCounter = ss.storage.visitCounter + 1;
+        makePanel();
     }
 }
 
@@ -17,10 +33,7 @@ function makePanel() {
         contentScriptWhen: "end",
         contentScriptFile: "./../lib/counter.js"
     });
-    panel.port.emit("counter", preferences.visitCounter);
+    panel.port.emit("counter", ss.storage.visitCounter);
     panel.show();
     setTimeout(function() {panel.hide();panel.destroy();}, preferences.timeout * 1000);
 }
-
-//When the counter increments in checkPage, show the panel for the timeout time.
-require("sdk/simple-prefs").on("visitCounter", makePanel);
